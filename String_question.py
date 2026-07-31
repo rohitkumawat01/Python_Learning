10c4234ea56b9f7ebc342fe8a3ba3c3986911d14
Rohit Kumawat <rkumawat7909@gmail.com>
2026-07-31 23:01:22 +0530

    Add files via upload

---
+++ String_question.py
@@ -0,0 +1,32 @@
+# " String"
+
+# Question 1:-
+
+# a=input("Enter your name :-")
+# print("Good Afternoon " +a)
+
+# Question 2:-
+
+# letter='''
+#         Dear <|Rohit|>,
+#         You are selected!
+#         <|date|>
+#           '''
+# print(letter.replace("<|Rohit|>", "Rk").replace("  <|date|>", "22 january"))
+
+# #Question 3 :- 
+
+# a="roh  it"
+# print(a[:5])
+
+#Question 4 :- 
+# name=" Hello Rohit  HOw are   you?"
+# print(name.find("  "))
+
+# Question 5 :- 
+# name=" Hello Rohit  HOw are you?"
+# print(name.replace("  "," "))
+
+letter="Dear Harry,\n\t This python course is nice.\n Thanka!"
+print(letter)
+
