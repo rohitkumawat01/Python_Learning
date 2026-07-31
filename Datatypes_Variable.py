0bd286e843e39867f1a9c40a9691d66eda2d6f2a
Rohit Kumawat <rkumawat7909@gmail.com>
2026-07-31 22:54:43 +0530

    Data types and variables in the python

---
+++ Datatypes_Variable.py
@@ -0,0 +1,52 @@
+# Python me code with harry se phad raha  hu jo ki 10 hr ki  class hai. isme hum single line comment ko # is se comment karte hai.
+
+'''ager multiple line comment hus to: 
+   rohit
+kuma
+i am 20 year old'''
+
+
+# Data types and variables in python
+
+# Data types
+            #  integer , float, string , bollean,none
+
+            # Variable :- 
+                        #  it is a name given to a memory location 
+
+ # Arithmetic Operator :- 
+
+# a=2
+# b=5
+# print(a*b)
+# print(2/5)
+# print(2-5)
+# print(2+5)
+
+#assingment operator:- 
+# a =10
+# a+=5
+
+# a-=5
+# print(a)
+
+# Comparison operator :- 
+ 
+# a=5<10
+# a=10<4
+# print(a)
+
+# Logical opereator :  
+
+# a=True or False
+# print(a)
+
+# b= True and False
+# print(b)
+
+# a=type("Rohit")
+# print(a)
+
+# a=input("Enter the name  " )
+# print("Hello ! " +a)
+
