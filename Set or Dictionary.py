7571c4ec2340bb0ab6b6e9339bc831b219cfc60d
Rohit Kumawat <rkumawat7909@gmail.com>
2026-08-06 22:32:27 +0530

    Add files via upload

---
+++ Set or Dictionary.py
@@ -0,0 +1,76 @@
+# Question 1
+
+# words={
+#     "madad": "help",
+#     "kursi": "Chair",
+#     "kutta": "Dog"
+# }
+# word=input("Enter the name you want :- " )
+# print(words[word])
+
+
+# Question 2 
+
+# s =set()
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# A=input("Enter the number ")
+# s.add(int(A))
+# print(s)
+
+# Question 3 
+
+# s=set()
+# s.add("18")
+# s.add(18)
+# print(s)
+
+# qUESTION 
+
+# s=set()
+# s.add(20)
+# s.add(20.0) # Length of operator of these operations ?
+# s.add('20')
+# print(len(s)) 
+
+# Question 
+
+# d={}
+
+# name=(input("Enter the name :- "))
+# lang=(input("Enter the language name :- "))
+# d.update({name: lang})
+
+# name=(input("Enter the name :- "))
+# lang=(input("Enter the language name :- "))
+# d.update({name: lang})
+
+# name=(input("Enter the name :- "))
+# lang=(input("Enter the language name :- "))
+# d.update({name: lang})
+
+# name=(input("Enter the name :- "))
+# lang=(input("Enter the language name :- "))
+# d.update({name: lang})
+
+# lang=(input("Enter the language name :- "))
+# d.update({name: lang})
+# print(d)  
+
+# Question 
+
+
