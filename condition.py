0e26b6b03df81a008c14b007195f3faaa5e77d30
Rohit Kumawat <rkumawat7909@gmail.com>
2026-08-17 20:08:48 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,42 @@
+# Question 
+# a1=int(input("Enter the number :- "))
+# a2=int(input("Enter the number :- "))
+# a3=int(input("Enter the number :- "))
+# a4=int(input("Enter the number :- "))
+
+# if(a1>a2 and a1>a2 and a1>a3 and a1>a4):
+#     print("a1 is the greatest number ")
+
+
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("a2 is the greatest")
+
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print("a3 is the greatest")
+
+# else:
+#     print("a4 is greatest")
+
+# print("Thank You ! ")
+
+#Question 
+
+# m1=int(input("Enter the subject marks1:- "))
+# m2=int(input("Enter the subject marks2:- "))
+# m3=int(input("Enter the subject marks3:- "))
+
+# total_persentage=(100*(m1+m2+m3))/300
+# if(total_persentage>=40 and m2>=33 and m2>=33 and m3>=33):
+#     print("You are pass in this year ")
+# else:
+#     print("Try again next year")
+
+
+#Question 3 
+
+# a=input("Enter the word")
+# if(a="make money" , "buy now" ,  "subscribe"  , "click here" )
+#     print("Spam !!!!!!!")
+
+# else:
+#     print("You are safe")   
