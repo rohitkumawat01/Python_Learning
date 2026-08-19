0c8b340d4d28f1e4f5a7af7369e042fe1097ff01
Rohit Kumawat <rkumawat7909@gmail.com>
2026-08-19 20:26:43 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,92 @@
+# # a=int(input("ENTER THE NUMBER FOR TABLE:- " ))
+# # for i in range (1,11):
+
+# #  print(a, '*',i,'=',a*i)
+
+# # Question 2 
+
+# # List=["Sohan","shlok","sonu","rohit "]
+# # for i in List:
+
+# #     if i.lower().startswith("s"):
+
+# #      print("Hello",i)
+
+# # Question 3
+
+# # n=int(input("Enter the number for the table:- " ))
+# # i=1
+# # while i<=10:
+    
+# #     print(i*n)
+# #     i +=1
+    
+    
+
+# #Question 4
+
+# # a=int(input("Enter the number :- "))
+
+# # if(a%2==0):
+
+# #      print("Even number")
+# # else :
+# #        print("IT is not a Prime number")
+       
+
+# # Question 5
+ 
+# # n = int(input("Enter the number : "))
+
+# # i = 1
+# # sum = 0
+
+# # while i <= n:
+# #     sum = sum + i
+# #     i += 1
+
+# # print("Sum =", sum)
+
+# #Question 6
+
+
+# # a=int(input("Enter the number:- "))
+# # i=1
+# # for f in range(1, a + 1):
+# #     i=i*f 
+# # print("Factorial =",i)
+    
+# #Question 7
+
+# # n=int(input("Enter the number:-"))
+# # i=1
+# # for a in range(1,n+1):
+
+# #  print("*" * i)
+# #  i=i+2
+
+# # Question 8
+# # n = int(input("Enter the number: "))
+
+# # for i in range(1, n + 1):
+# #     print(" " * (n - i) + "*" * (2 * i - 1))
+
+
+# # Question 9 
+# # n=int(input("Enter the number "))
+# # for i in range(1,n+1):
+
+# #     if(i==1 or i==n):
+# #         print("*"*n,end="")
+# #     else:
+# #          print("*",end="")
+# #          print(" " *(n-2), end="")
+# #          print("*",end="")
+# #     print("") 
+
+# # Question 10 
+
+# a=int(input("ENTER THE NUMBER FOR TABLE:- " ))
+# for i in range (1,11):
+
+#  print(a, '*',11-i,'=',a*(11-i))
