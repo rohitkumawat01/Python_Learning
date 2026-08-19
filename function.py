b65a0a8b33a0a8747a1d8c2bf5580a1882347e33
Rohit Kumawat <rkumawat7909@gmail.com>
2026-08-19 20:27:13 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,83 @@
+# def name():
+#     name=input("ENter your name :- ")
+#     print(name)
+# name()
+
+# def goodday(name,thank):
+#     print("Radhe Radhe," +name)
+#     print(thank)
+
+# goodday("rohit","thank you baby")
+
+
+
+# Question 
+
+
+# def greatest(a,b,c):
+#     if(a>b and a>c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c
+# a=89
+# b=56
+# c=55
+# print(greatest(a,b,c))
+
+# Question 2
+
+# def f_to_c(f):
+#     return 5*(f-32)/9
+# f= int(input("Enter the Temprature:-"))
+# c=f_to_c(f)
+# print(f"{round(c,3)} c")
+
+# # Question 3 
+
+# def sum(n):
+#     if(n==1):
+#         return 1;
+#     return sum(n-1)+n
+# print(sum(4))
+ 
+
+# qUESTION
+
+# def pattern(n):
+#     if(n==0):
+#         return
+#     print("*"*n)
+#     pattern(n-1)
+
+# pattern(5)
+
+
+# @qeestion
+# def inch_to_cms(inch):
+#     return inch*2.54
+# n=int(input("Enter the inch value :-"))
+# print(f"The coresponding value in cms is:-{inch_to_cms(n)}")
+
+
+#question 
+
+# def rem(l,word):
+#     n=[]
+
+#     for item in l:
+#         if not(item==word):
+#             n.append(item.strip(word))
+#     return n
+
+# l=["harry","rohan","shubham","an"]
+# print(rem(l,"an")) 
+
+
+# question 
+# def multi(n):
+#     for i in range(1,11):
+#         print(f"{n} x {i}={n*i}")
+# multi(5)
+             
